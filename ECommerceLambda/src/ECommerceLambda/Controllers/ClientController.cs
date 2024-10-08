cec8e4757d1bf6876f36bea75abdbf72f39f801e
Fernando Moutinho <fernandomvmoutinho@gmail.com>
2024-10-08 00:07:46 -0300

    Implement ClientController

--- ECommerceLambda/src/ECommerceLambda/Controllers/ClientController.cs
+++ ECommerceLambda/src/ECommerceLambda/Controllers/ClientController.cs
@@ -8,29 +8,41 @@ namespace ECommerceLambda.Controllers
     public class ClientController : ControllerBase
     {
         private readonly ILogger<ClientController> _logger;
+        private readonly IClientRepository _clientRepository;
 
-        public ClientController(ILogger<ClientController> logger)
+        public ClientController(ILogger<ClientController> logger, IClientRepository clientRepository)
         {
             _logger = logger;
+            _clientRepository = clientRepository;
         }
         [HttpPost]
         public async Task<IActionResult> CreateClient(Client client)
         {
+            await _clientRepository.Create(client);
             return Ok();
         }
+
         [HttpGet("{document}")]
         public async Task<IActionResult> GetClientByDocument(string document)
         {
-            return Ok();
+            var client = await _clientRepository.GetByDocument(document);
+            
+            if (client == null)
+                return NotFound();
+
+            return Ok(client);
         }
+
         [HttpPut]
         public async Task<IActionResult> UpdateClient(Client client)
         {
+            await _clientRepository.Update(client);
             return Ok();
         }
         [HttpDelete("{document}")]
         public async Task<IActionResult> DeleteClient(string document)
         {
+            await _clientRepository.Delete(document);
             return Ok();
         }
     }
